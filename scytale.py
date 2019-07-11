669fd976028d2a2be74a65e8379696876a661b0e
Jan Schmitt <jan@smittie.de>
2019-07-11 18:01:04 +0200

    changed naming to scytale. added cli menu when starting script from shell.

---
+++ scytale.py
@@ -0,0 +1,67 @@
+import pyfiglet
+import math
+
+def scytale_encode(input, diameter):
+    input = str(input)
+    out = str()
+
+    for y in range(0,diameter):
+        out += input[y::diameter]
+
+    return out
+
+
+def scytale_decode(input, diameter):
+    input = str(input)
+    out = str()
+
+    if diameter > 1:
+        diameter = math.ceil(len(input)/diameter)
+
+    for y in range(0,diameter):
+        out += input[y::diameter]
+
+    return out
+
+
+def main_menu(user_input, menu):
+    mode = ["encoded", "decoded"] 
+
+    while user_input !=2:
+        print(menu)
+        try:
+            user_input = int(input("Enter your selection:[0] \n"))
+        except: 
+            user_input = 0
+        
+        if user_input == 2:
+            break
+        elif user_input == 0:
+            user_string = str(input(f"\nPlease enter a string to be {mode[user_input]}: \n"))
+            try:
+                user_diameter = int(input("\nPlease enter a diameter:[2] \n"))
+            except:
+                user_diameter = 2
+            print("\nYour encoded string:\n" + scytale_encode(user_string, user_diameter) + "\n")
+        else:
+            user_string = str(input(f"\nPlease enter a string to be {mode[user_input]}: \n"))
+            try:
+                user_diameter = int(input("\nPlease enter a diameter:[2] \n"))
+            except:
+                user_diameter = 2
+            print("\nYour decoded string:\n" + scytale_decode(user_string, user_diameter) + "\n")
+
+
+def welcome_user():
+    entry_banner = pyfiglet.figlet_format("Scytale", font='isometric1')
+    menu = """What do you wanna do?\n
+    [0] Encode a given string (default)\n
+    [1] Decode a given string\n
+    [2] I wanna go to Rio (exit)\n"""
+
+    print(entry_banner)
+    main_menu(0, menu)
+    return print(pyfiglet.figlet_format("\nCya next time! Bye!",font='digital'))
+        
+if __name__ == '__main__':
+    welcome_user()
--- test_scytale.py
+++ test_scytale.py
@@ -1,108 +1,108 @@
 import unittest
-from skytale import skytale_encode, skytale_decode
+from scytale import scytale_encode, scytale_decode
 
-class SkytaleTests(unittest.TestCase):
+class scytaleTests(unittest.TestCase):
     def test_equal_io_encode(self):
         """the length of the character sequence shouldn't change during encoding"""
         self.assertEqual(
-            len(skytale_encode("hallo sipgate", 1)),
+            len(scytale_encode("hallo sipgate", 1)),
             len("hallo sipgate")
         )
         self.assertEqual(
-            len(skytale_encode("hallo welt!", 6)),
+            len(scytale_encode("hallo welt!", 6)),
             len("hwaelllto! ")
         )
 
     def test_seq_encode_min(self):
         """sequence with diameter 1 should result in the same output"""
         self.assertEqual(
-            (skytale_encode("sipgate hacking talents!", 1)),
+            (scytale_encode("sipgate hacking talents!", 1)),
             "sipgate hacking talents!"
         )
         self.assertEqual(
-            (skytale_encode("hallo welt!", 1)),
+            (scytale_encode("hallo welt!", 1)),
             "hallo welt!"
         )
 
     def test_even_seq_encode(self):
         """sequence with diameter > 1 should decode seq"""
         self.assertEqual(
-            (skytale_encode("sipgate hacking talents!", 6)),
+            (scytale_encode("sipgate hacking talents!", 6)),
             "seili nephgnga tactstka!"
         )
         self.assertEqual(
-            (skytale_encode("sipgate hacking talents!", 5)),
+            (scytale_encode("sipgate hacking talents!", 5)),
             "stc niekttp iasghnl!aage"
         )
 
     def test_odd_seq_encode(self):
         """odd sequences shouldn't break the program and be unrelated to diameter"""
         self.assertEqual(
-            (skytale_encode("hallo welt!", 6)),
+            (scytale_encode("hallo welt!", 6)),
             "hwaelllto! "
         )
         self.assertEqual(
-            (skytale_encode("hallo welt!", 5)),
+            (scytale_encode("hallo welt!", 5)),
             "h !awlellot"
         )
 
     def test_null_seq_encode(self):
         """no input shouldn't break the program"""
         self.assertEqual(
-            (skytale_encode("", 6)),
+            (scytale_encode("", 6)),
             ""
         )
     
     def test_numeric_input_encode(self):
         """numeric inputs should be treated as strings"""
         self.assertEqual(
-            (skytale_encode(2, 6)),
+            (scytale_encode(2, 6)),
             "2"
         )
 
     def test_equal_io_decode(self):
         """the length of the character sequence shouldn't change during decoding"""        
         self.assertEqual(
-            len(skytale_decode("hallo sipgate", 1)),
+            len(scytale_decode("hallo sipgate", 1)),
             len("hallo sipgate")
         )
         self.assertEqual(
-            len(skytale_decode("hallo welt!", 6)),
+            len(scytale_decode("hallo welt!", 6)),
             len("hwaelllto! ")
         )
 
     def test_even_seq_decode_min(self):
         """sequence with diameter = 1 should result in the same output"""
         self.assertEqual(
-            (skytale_decode("sipgate hacking talents!", 1)),
+            (scytale_decode("sipgate hacking talents!", 1)),
             "sipgate hacking talents!"
         )
 
     def test_even_seq_decode(self):
         """sequence with diameter > 1 should be decoded as expected"""
         self.assertEqual(
-            (skytale_decode("seili nephgnga tactstka!", 6)),
+            (scytale_decode("seili nephgnga tactstka!", 6)),
             "sipgate hacking talents!"
         )
 
     def test_odd_seq_decode(self):
         """odd sequences shouldn't break the decoding"""
         self.assertEqual(
-            (skytale_decode("hwaelllto! ", 6)),
+            (scytale_decode("hwaelllto! ", 6)),
             "hallo welt!"
         )
     
     def test_null_seq_decode(self):
         """no input shouldn't break the decoding"""
         self.assertEqual(
-            (skytale_decode("", 6)),
+            (scytale_decode("", 6)),
             ""
         )
     
     def test_numeric_input_decode(self):
         """numeric inputs should be treated as strings"""
         self.assertEqual(
-            (skytale_decode(2, 6)),
+            (scytale_decode(2, 6)),
             "2"
         )
 
