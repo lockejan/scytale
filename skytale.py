bfe3aac5edee149717d3cdd6302e9c2cbfe8e146
Jan Schmitt <jan@smittie.de>
2019-07-11 15:43:13 +0200

    added decoding fn. added use of math package

--- skytale.py
+++ skytale.py
@@ -1,4 +1,6 @@
 
+import math
+
 def skytale_encode(input, diameter):
     input = str(input)
     out = str()
@@ -9,4 +11,10 @@ def skytale_encode(input, diameter):
     return out
 
 def skytale_decode(input, diameter):
-    pass
+    input = str(input)
+    diameter = math.ceil(len(input)/2)
+    out = str()
+
+    for y in range(0,diameter):
+        out += input[y::diameter]
+    return out
