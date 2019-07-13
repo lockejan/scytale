a00ada5460765a8d10939b72eabed9154c8a0bd7
Jan Schmitt <jan@smittie.de>
2019-07-13 12:05:48 +0200

    updated formatting with autopep8

--- scytale.py
+++ scytale.py
@@ -1,10 +1,11 @@
 import pyfiglet
 from math import ceil
 
+
 def scytale_process(input, diameter):
     out = str()
 
-    for y in range(0,diameter):
+    for y in range(0, diameter):
         out += input[y::diameter]
     return out
 
@@ -28,18 +29,20 @@ def main_menu(user_input, menu):
             if user_input not in ["", "0"]:
                 print(f"\n'{user_input}' is not a number. Continuing with 0.\n")
             user_input = 0
-        
+
         if user_input == 2:
             break
-        elif user_input not in [0,1]:
+        elif user_input not in [0, 1]:
             print(f"\n'{user_input}' is not a valid entry. Try again!\n")
         else:
-            user_string = str(input(f"\nPlease enter a string to be {mode[user_input]}: \n"))
+            user_string = str(
+                input(f"\nPlease enter a string to be {mode[user_input]}: \n"))
             try:
                 user_diameter = int(input("\nPlease enter a diameter:[2] \n"))
             except:
                 user_diameter = 2
-            print(f"\nYour {mode[user_input]} string:\n '" + scytale_init(user_string, user_diameter, user_input) + "'\n")           
+            print(f"\nYour {mode[user_input]} string:\n '" +
+                  scytale_init(user_string, user_diameter, user_input) + "'\n")
 
 
 def welcome_user():
@@ -51,8 +54,8 @@ def welcome_user():
 
     print(entry_banner)
     main_menu(0, menu)
-    return print(pyfiglet.figlet_format("\nCya next time! Bye!",font='digital'))
+    return print(pyfiglet.figlet_format("\nCya next time! Bye!", font='digital'))
 
-        
+
 if __name__ == '__main__':
     welcome_user()
