a599894a4ca3bb672c1e2e0c50456bff10936e24
Jan Schmitt <git@smittie.de>
2025-06-07 17:31:51 +0200

    fix(flake): update flake file to a workable state

--- flake.nix
+++ flake.nix
@@ -2,17 +2,24 @@
   description = "Application packaged using poetry2nix";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, poetry2nix }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      p2n = poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+
       package = "scytale";
     in
     {
       # This block describes the build instructions for the default pkg
-      packages.${system}.default = pkgs.poetry2nix.mkPoetryApplication {
+      packages.${system}.default = p2n.mkPoetryApplication {
         projectDir = ./.; # self also works here
       };
 
@@ -24,8 +31,10 @@
         # This will add our package to the dev environment
         packages = with pkgs; [
           # creates and enters a poetry environment
-          (poetry2nix.mkPoetryEnv { projectDir = ./.; })
-          python3
+          (p2n.mkPoetryEnv {
+            projectDir = ./.;
+          })
+          poetry
           sphinx
         ];
       };
